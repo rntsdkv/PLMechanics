48f012a44a913c98295f03fccbc71326c8d33147
Renat <renat.sadykovrenat@yandex.ru>
2020-07-12 19:37:43 +0300

    Создаем класс для /trade

---
+++ src/main/java/ru/prisonlife/plmechanics/commands/Trade.java
@@ -0,0 +1,17 @@
+package ru.prisonlife.plmechanics.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author rntsdkv
+ * @project PLMechanics
+ */
+
+public class Trade implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return true;
+    }
+}

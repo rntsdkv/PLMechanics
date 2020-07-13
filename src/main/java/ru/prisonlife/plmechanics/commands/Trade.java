83a07fd90c4a45d522151f0360e666ed249206a9
Renat <renat.sadykovrenat@yandex.ru>
2020-07-13 11:33:05 +0300

    Добавляем сообщение о предложении трейда

--- src/main/java/ru/prisonlife/plmechanics/commands/Trade.java
+++ src/main/java/ru/prisonlife/plmechanics/commands/Trade.java
@@ -1,5 +1,8 @@
 package ru.prisonlife.plmechanics.commands;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -56,7 +59,16 @@ public class Trade implements CommandExecutor {
         }
 
         trades.add(new Trading(player, t));
-        // TODO дальше
+
+        t.sendMessage(colorize("&l&bИгрок &1" + player.getName() + "&b предлагает вам трейд"));
+        TextComponent textComponent = new TextComponent(colorize("&l&2Принять"));
+        textComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(colorize("&l&2Принять трейд"))));
+        textComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/phoneaccept true"));
+        TextComponent textComponent1 = new TextComponent(colorize("&l&cОтменить"));
+        textComponent1.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(colorize("&l&2Отменить трейд"))));
+        textComponent1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/phoneaccept false"));
+        t.spigot().sendMessage(textComponent);
+        t.spigot().sendMessage(textComponent1);
         return true;
     }
 }
